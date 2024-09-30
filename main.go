47e4d660d595b4cc61bc9ef23c1d270e52085303
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-30 22:07:24 +0530

    feat: qr colours

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/skip2/go-qrcode"
@@ -9,7 +10,8 @@ import (
 func main() {
 	//var png []byte
 	//png, err := qrcode.Encode("https://github.com/Oik17", qrcode.Medium, 256)
-	err := qrcode.WriteFile("https://github.com/Oik17", qrcode.Medium, 256, "qr.png")
+
+	err := qrcode.WriteColorFile("https://github.com/Oik17", qrcode.Medium, 256, color.Black, color.White, "qr.png")
 
 	if err != nil {
 		log.Println(err)
